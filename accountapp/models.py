6b287554a5111370104a6975645d258c05156e97
SUNGMIN KIM <obingguo@naver.com>
2021-07-07 10:59:42 +0900

    2021/07/07 1ban class commit 3

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+# models 라는 모듈안에 Model 클래스를 불러오는 것
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
