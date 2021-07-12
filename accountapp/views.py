391232edb4732b27470b7f60f308f11f2d878f05
SUNGMIN KIM <obingguo@naver.com>
2021-07-12 16:03:53 +0900

    2021/07/12 1ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
 from accountapp.models import HelloWorld
 
 
@@ -15,11 +20,18 @@ def hello_world(request):
         new_hello_world.save()
 
         hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'hello_world_list': hello_world_list})
 
 #accountapp/hello_world.html
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world') # 23번줄 return HttpResponseRedirect(reverse('accountapp:hello_world')) 에서는 reverse 로 바로 받았지만 여기선 lazy가 붙는다. 바로 작동하는게 아니라 나중에 작동하기때문에.
+    template_name = 'accountapp/create.html'
