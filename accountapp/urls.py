69c090312a4752ca53c64ff66da563b00dde036f
SUNGMIN KIM <obingguo@naver.com>
2021-07-15 14:52:53 +0900

    2021/07/15 1ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
-    path('create/', AccountCreateView.as_view(), name='create') # 위에선 함수형 view 이기 때문에 hello_world로 바로 불러오지만. 클래스형 view를 가져올때는 as_view를 붙여야한다. 이정도만 알자.
+    path('create/', AccountCreateView.as_view(), name='create')
+    # 위에선 함수형 view 이기 때문에 hello_world로 바로 불러오지만. 클래스형 view를 가져올때는 as_view를 붙여야한다. 이정도만 알자.
 ]
