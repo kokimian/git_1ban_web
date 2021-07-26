317e4de3a993d32300fe5ce30a943547f379b8bb
SUNGMIN KIM <obingguo@naver.com>
2021-07-26 14:53:22 +0900

    2021/07/26 1ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -65,7 +65,7 @@ class AccountUpdateView(UpdateView):
 
     # def get(self, request, *args, **kwargs):
     #     if request.user.is_authenticated and self.get_object() == request.user: # and 문 추가. 로그인만 되어있으면 다른 사용자의 페이지에 접속 가능한 것을 막기위해.
-    #         return super().get(request, *args, **kwargs)                        # get_object()는 target_user와 동일하다고 보면 무방
+    #         return super().get(request, *args, **kwargs)    # self.get_object() : self는 현재 AccountUpdateView 에서 사용되고있는 object 즉, urls에서 보면 <int:pk> 를 받는다. pk에 해당되는 object를 가져온다. 만약 6번이라고 하면 (update/6) 그 객체를 가져오는 것. 즉, target_user와 동일하다고 보면 무방
     #     else:
     #         return HttpResponseForbidden()
     #
