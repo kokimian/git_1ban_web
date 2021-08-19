250b5d43f72d27973870f0bd1e77c548e9b913b4
SUNGMIN KIM <obingguo@naver.com>
2021-08-19 14:45:42 +0900

    2021/08/19 1ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
@@ -15,6 +16,9 @@ from accountapp.models import HelloWorld
 
 
 # @login_required(login_url=reverse_lazy('accountapp:login'))
+from articleapp.models import Article
+
+
 @login_required
 def hello_world(request):
 
@@ -50,10 +54,17 @@ class AccountCreateView(CreateView):
         return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(writer=self.object)
+        return super().get_context_data(object_list=article_list,
+                                        **kwargs)
 
 
 has_ownership = [account_ownership_required, login_required]
