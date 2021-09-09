ed7339e9bdfd8c9dafafc5bb9724fa2b6dc5a96b
SUNGMIN KIM <obingguo@naver.com>
2021-09-09 23:24:54 +0900

    9/9 djangoProject commit 2 profileapp fix

--- profileapp/views.py
+++ profileapp/views.py
@@ -11,10 +11,11 @@ from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
-@method_decorator(profile_ownership_required, 'get')
-@method_decorator(profile_ownership_required, 'post')
+@method_decorator(login_required(), 'get')
+@method_decorator(login_required(), 'post')
 class ProfileCreateView(CreateView):
     model = Profile
+    context_object_name = 'target_profile'
     form_class = ProfileCreationForm
     #success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
@@ -25,6 +26,7 @@ class ProfileCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 
 # accounts 로 설정 안한사람은 login_required(안에 주소)커스텀해야한다
 @method_decorator(profile_ownership_required, 'get')
@@ -38,3 +40,4 @@ class ProfileUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
 #self.object가 target_user라고 생각해야함 profileapp의 유저가 아니라 accountapp의 유저로 가야하므로 .user필요
+
